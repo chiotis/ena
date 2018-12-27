9e542327791e3507494e567b26786ce3c138955b
Christos Chiotis <chiotis@gmail.com>
2018-12-27 09:16:26 +0200

    Create offcanvas.php

---
+++ components/offcanvas.php
@@ -0,0 +1,9 @@
+<!-- This is the off-canvas -->
+<div id="offcanvas" uk-offcanvas="overlay: true; mode: <?php the_field('offcanvas_mode', 'options'); ?>; flip: <?php the_field('offcanvas_position', 'options'); ?>;">
+	<div class="uk-offcanvas-bar">
+		<button class="uk-offcanvas-close" type="button" uk-close></button>
+		<h3>Title</h3>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+	</div>
+</div>
